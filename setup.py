e3af0b8ea4db054e02fc4351f9a6dad7598d1ea4
Eterna2 <eterna2@hotmail.com>
2018-07-02 12:44:36 +0800

    Release v0.0.3

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@
 """Setup `databricks_utils` package"""
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r") as fh:
+    README = fh.read()
+
 setup(
     name='databricks-utils',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/e2fyi/databricks-utils',
     license='Apache License 2.0',
     author='William Teo',
     author_email='eterna2@hotmail.com',
     description='Ease-of-use utility tools for databricks notebooks.',
+    long_description=README,
+    long_description_content_type="text/markdown",
     packages=find_packages(include=['databricks_utils*'],
                            exclude=["notebooks*", "docs*", "*.sh"]),
     include_package_data=False,
